b19ef6a411f587eed832beb6d736ef710697121d
siddhartha-12 <siddhartharaju12@gmail.com>
2020-12-30 07:50:57 -0500

    Adding Header reusability in Home and About pages

--- agency-website/src/App.js
+++ agency-website/src/App.js
@@ -1,12 +1,30 @@
-import react,{Component} from 'react'
+import react, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import PageWrapper from './components/PageWrapper'
+
+//Pages
 import Home from './components/Pages/Home'
+import About from './components/Pages/About'
 
 function App() {
   return (
-    <PageWrapper>
-      <Home />
+    <Router>
+      <PageWrapper>
+        <Route
+          path="/home"
+          component={Home}
+        />
+
+        <Route
+          path="/about"
+          component={About}
+        />
+      <Route
+          path="/"
+          component={Home}
+        />
       </PageWrapper>
+    </Router>
   );
 }
 
--- agency-website/src/components/Pages/Home.js
+++ agency-website/src/components/Pages/Home.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../Common/Header';
+import image from '../assets/img/header-bg.jpg';
 
 class Home extends Component {
     render() {
         return (
             <div>
-                <header className="masthead">
-                    <div className="container">
-                        <div className="masthead-subheading">Welcome To Our Studio!</div>
-                        <div className="masthead-heading text-uppercase">It's Nice To Meet You</div>
-                        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-                    </div>
-                </header>
+                <Header
+                    title="Welcome To Our Studio!"
+                    subtitle="IT'S NICE TO MEET YOU"
+                    buttonText="Tell Me More"
+                    link="/services"
+                    showButton={true}
+                    image={image}
+                />
             </div>
         )
     }
